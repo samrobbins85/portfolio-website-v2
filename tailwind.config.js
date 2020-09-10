388372af1a9b126bf8d46afc9639d48c7b98fe53
Sam Robbins <samrobbinsgb@gmail.com>
2020-09-10 12:29:03 +0100

    Ready for production

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
     },
   },
   variants: {
-    display: ["group-hover"],
+    display: ["responsive", "group-hover"],
   },
   plugins: [require("@tailwindcss/typography")],
 };
