59546fe9274ffd51dfe81b1aca1e0fafe4d6a929
Sam Robbins <samrobbinsgb@gmail.com>
2020-12-02 09:20:56 +0000

    Update to use my npm package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
-const round = (num) =>
-  num
-    .toFixed(7)
-    .replace(/(\.[0-9]+?)0+$/, "$1")
-    .replace(/\.0$/, "");
-const rem = (px) => `${round(px / 16)}rem`;
 const colors = require("tailwindcss/colors");
 module.exports = {
+  presets: [require("@samrobbins/typography")],
   purge: {
     content: [
       "./themes/tailwind-developer-website/layouts/**/*.html",
@@ -23,33 +18,6 @@ module.exports = {
         gray: colors.gray,
         "cool-gray": colors.coolGray,
       },
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            "code::after": {
-              content: "none",
-            },
-            "code::before": {
-              content: "none",
-            },
-            "pre code::after": {
-              content: "none",
-            },
-            code: {
-              backgroundColor: theme("colors.gray.200"),
-              color: theme("colors.gray.700"),
-              padding: "4px",
-              borderRadius: rem(2),
-            },
-            blockquote: {
-              quotes: "none",
-            },
-            pre: {
-              backgroundColor: theme("colors.cool-gray.800"),
-            },
-          },
-        },
-      }),
     },
 
     fontFamily: {
@@ -71,5 +39,4 @@ module.exports = {
   variants: {
     display: ["responsive", "group-hover"],
   },
-  plugins: [require("@tailwindcss/typography")],
 };
